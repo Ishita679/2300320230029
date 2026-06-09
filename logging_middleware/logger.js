0fae61147e8a7a65d078716c41285df167dc3a09
Ishita679 <ishuagrawal864@gmail.com>
2026-06-09 12:01:53 +0530

    updated logger.js

--- logging_middleware/logger.js
+++ logging_middleware/logger.js
@@ -1,8 +1,24 @@
-const logger = (req, res, next) => {
-  console.log(
-    `${new Date().toISOString()} | ${req.method} | ${req.originalUrl}`
-  );
-  next();
-};
+const axios = require("axios");
+const LOG_API = "http://4.224.186.213/evaluation-service/logs";
+const AUTH_TOKEN = "cXuqht";
+async function Log(stack, level, pkg, message) {
+  try {
+    const payload = {
+      stack,
+      level,
+      package: pkg,
+      message,
+    };
 
-module.exports = logger;
+    await axios.post(LOG_API, payload, {
+      headers: {
+        Authorization: `Bearer ${AUTH_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.error("Failed to send log:", error.message);
+  }
+}
+
+module.exports = Log;
